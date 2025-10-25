74d41601fd9d4644e9f84ebc504b3e7283fa538f
Lorenzo <jstam472@gmail.com>
2025-10-25 00:02:39 -0600

    more methods added

--- Comp_1502_Assignment_2-HaseebBranch/src/mru/tsc/controller/ToyStoreManager.java
+++ Comp_1502_Assignment_2-HaseebBranch/src/mru/tsc/controller/ToyStoreManager.java
@@ -7,7 +7,53 @@ import mru.tsc.model.Toy;
 public class ToyStoreManager {
 
 	
-
+	private void selectLogic (ArrayList<Toy> searchedList) {
+		int maxSize; //WIP
+		
+		maxSize = searchedList.size(); //WIP
+		
+		
+	}
+	
+	/**
+	 * This method decrements the item that the user wants to purchase if there is at least one count of that object
+	 * 
+	 * @param toy the toy that the user chooses to buy
+	 */
+	private void purchase(Toy toy) {
+		if (toy.getCount() > 0) {
+			toy.decrement(); //if theres at least one toy available and the user wants to purchase then remove one count from the DB
+			System.out.println("Succesfully Purchased"); //temporary placeholder to call menu class
+		}
+		else System.out.println("Error: out of stock"); //temporary placeholder to call menu class
+	}
+	
+	
+	/**
+	 * This method displays the list of toys that matches the users search by calling the menu class to print
+	 * 
+	 * @param searchedList the list containing the toys in the database as an ArrayList Class
+	 */
+	private void displayToyList(ArrayList<Toy> searchedList) {
+		int maxSize;
+		
+		maxSize = searchedList.size(); //WIP
+		
+		for (Toy toy : searchedList) {
+			System.out.println(toy.toString()); // TEMPORARY PLACEHOLDER TO CALL MENU CLASS
+			
+		}
+	}
+	
+	/**
+	 * This method displays a single of toys that matches the users search by calling the menu class to print
+	 * 
+	 * @param singleToy A single toy that will be displayed as a Toy class
+	 */
+	private void displayToy(Toy singleToy) {
+		System.out.println(singleToy.toString()); // TEMPORARY PLACEHOLDER TO CALL MENU CLASS
+		
+	}
 	
 	/**
 	 * This method compares the given serial number to all serial number in the database and returns a matching serial number
@@ -15,19 +61,17 @@ public class ToyStoreManager {
 	 * @param serialNumber the serial number given by the user
 	 * @return the toy object
 	 */
-	private Toy compareSNToListDb(String serialNumber) {
+	private ArrayList<Toy> compareSNToAllToys(String serialNumber) {
+		ArrayList<Toy> toySNList = new ArrayList<Toy>();
 		String currentSN;
 		
 		for (Toy toy : toyList) { // read each object in list, then do a getSN to get the serial number
-			currentSN = toy.getSN;
+			currentSN = toy.getSN();
 			
-			if (serialNumber == currentSN) return toy;  //compare SN to database SN, if true return that toy
-			
-			else {
-				System.out.println("toy not found"); // PlaceHOLDER!!!!
-				return null; 
-			}
+			if (serialNumber == currentSN) toySNList.add(toy);  //compare SN to database SN, if true  add to a list to return
 		}
+		
+		return toySNList;
 		
 	}
 	
@@ -37,15 +81,15 @@ public class ToyStoreManager {
 	 * @param name the name given by the user
 	 * @return an arraylist that contains all toys with matching string sequence that was given by the user
 	 */
-	private ArrayList<Toy> compareNameToListName(String name) {
+	private ArrayList<Toy> compareNameToAllToys(String name) {
 		ArrayList<Toy> toyNameList = new ArrayList<Toy>();
 		
 		String currentName;
 		
 		for (Toy toy : toyList) { // read each object in list, then do a getName the Name
-			currentName = toy.getName;
+			currentName = toy.getName();
 			
-			if (name == currentName) toyNameList.add(toy);  //compare Name to database Name, if true  add to a list to return
+			if (currentName.contains(name)) toyNameList.add(toy);  //compare Name to database Name, if true  add to a list to return
 		}
 		
 		return toyNameList;
@@ -57,7 +101,7 @@ public class ToyStoreManager {
 	 * @param type the object type name that the user provides
 	 * @return a list of toys with the same object type
 	 */
-	private ArrayList<Toy> compareNameToListType (String type) { 
+	private ArrayList<Toy> compareTypeToAllToys (String type) { 
 		ArrayList<Toy> toyTypeList = new ArrayList<Toy>();
 		
 		String currentType;
