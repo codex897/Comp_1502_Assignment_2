8ccf6563ad6b1c5073cd2782f02fe53dcb6077b9
codex897 <ayose897@mtroyal.ca>
2025-11-09 23:30:23 -0700

    Test case figures

--- Comp_1502_Assignment_2-HaseebBranch/test/mru/test/model/FigureTest.java
+++ Comp_1502_Assignment_2-HaseebBranch/test/mru/test/model/FigureTest.java
@@ -1,14 +1,87 @@
 package mru.test.model;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import mru.tsc.model.Figure;
+
 
 class FigureTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    private Figure figure;
 
+    @BeforeEach
+    void initialize() {
+        figure = new Figure("1234567890", "ActionMan", "Hasbro", 29.99, 5, 8, 'A');
+    }
+
+    @Test
+    void testGetAndSetClassification() {
+        assertEquals('A', figure.getClassification());
+        figure.setClassification('H');
+        assertEquals('H', figure.getClassification());
+    }
+
+    @Test
+    void testTypeOf() {
+        assertEquals("Figure", figure.typeOf());
+    }
+
+    @Test
+    void testStringToFormat() {
+        String expected = "1234567890;ActionMan;Hasbro;29.99;5;8;A";
+        assertEquals(expected, figure.stringToFormat());
+    }
+
+    @Test
+    void testToString() {
+        String result = figure.toString();
+        assertTrue(result.contains("Classification"));
+        assertTrue(result.contains("A"));
+    }
+
+    @Test
+    void testGetAndSetName() {
+        assertEquals("ActionMan", figure.getName());
+        figure.setName("Knight");
+        assertEquals("Knight", figure.getName());
+    }
+
+    @Test
+    void testGetAndSetSerialNum() {
+        assertEquals("1234567890", figure.getSerialNum());
+        figure.setSerialNum("0987654321");
+        assertEquals("0987654321", figure.getSerialNum());
+    }
+
+    @Test
+    void testGetAndSetBrand() {
+        assertEquals("Hasbro", figure.getBrand());
+        figure.setBrand("Mattel");
+        assertEquals("Mattel", figure.getBrand());
+    }
+
+    @Test
+    void testGetAndSetPrice() {
+        assertEquals(29.99, figure.getPrice());
+        figure.setPrice(49.99);
+        assertEquals(49.99, figure.getPrice());
+    }
+
+    @Test
+    void testGetAndSetCount() {
+        assertEquals(5, figure.getCount());
+        figure.setCount(10);
+        assertEquals(10, figure.getCount());
+    }
+
+   
+
+    @Test
+    void testToyDecrement() {
+        int initialCount = figure.getCount();
+        int newCount = figure.toyDecrement();
+        assertEquals(initialCount - 1, newCount);
+    }
 }
